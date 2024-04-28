c4ab9e5ad92d0943e0ee70e3afab86677f174faa
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2024-04-28 12:08:30 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let answer: {
+  Sentence: string;
+} = await inquirer.prompt({
+  name: "Sentence",
+  type: "input",
+  message: "Enter your sentence to count words",
+});
+
+let wordCounter = answer.Sentence.trim().split(" ");
+console.log(wordCounter);
+
+console.log(`Your sentence word count is ${wordCounter.length}`);
